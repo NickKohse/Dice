20c481b9ed5e4a0ead31621ae59e1375023e2a64
NickKohse <nick.kohse@gmail.com>
2018-08-05 18:51:44 -0600

    started determine_strength function, moved hand_printer into class, added to readme

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Dice
+
+## Summary
+
+## Ranking of the Hands
+- 1. Straight - five consecutive values (1,2,3,4,5 or 2,3,4,5,6)
+- 2. Five of a kind - all dice show the same value
+- 3. Four of a kind
+- 4. Full House - three of a kind and a pair in the same hand
+- 5. Three of a kind
+- 6. Two pairs
+- 7. Pair
+- 8. Highest single value - The highest value of any of the die
+- 9. Highest sum - The sum of the values shown on all die
+
+## Hand Strength Determining Function
+The function used to determine which hand is stronger is by far the most complex part of this 
+program. It determines a strength value as an integer to represent each of the hands. This 
+makes the comparison of two competing hand very simple. Below is an explanation of how the 
+strength value is calculated.
+
+### Strength Value Calculation 
+ Sum of the value of the dice
++(50 * highest value die)
++(500 * value of a paired die)
++(1500 * value of die in second pair)
++(10,000 * value of tripled die)
++(100,000 * value of quadrupled die)
++(1,000,000 * value of quintupled die)
++(10,000,000 for straight)
+---------------------------------------
+= Strength value
+
+### Notes on Strength Value
+- The order of the die in each hand is irrelevant i.e. (5,5,6,1,2) is the same hand as (5,6,2,1,5)
+- The max theoretical value is 10,000,320 for this hand (2,3,4,5,6) 10M for straight + 300 for high die 6 + 20 for the sum of the die
+- The min theoretical value is 316 for this hand (1,2,3,4,6) 300 for high die 6 + 16 for the sum of the die
+- The min and max hands are only one die different, both have 2,3,4,6
+- The lowest possible value for each rank n is higher than the highest possible value for the rank n-1
+- For example, the highest score in rank 8 is 319 for this hand (1,3,4,5,6), the lowest score for rank 7 is 711 for this hand (1,1,2,3,4)
+- The strength value is an internal mechanism only, it is not shown to users of the game as it is not relevant 
+
+## Things to add
+- Ability to bet
+- Different difficulties
+- Error checking on input
--- dice.rb
+++ dice.rb
@@ -1,9 +1,11 @@
 
 class Dice_Hand
 		
-	def initialize
+	def initialize(name)
+		@name = name
 		@hand = Array.new(5)
 	end
+	
 	#initializes the hand to random values
 	def populate
 		for i in 0..4 do
@@ -14,7 +16,7 @@ class Dice_Hand
 	#re-rolls dice at the specified indexes
 	def reroll(indexes)
 		indexes.each do |i|
-		puts "RE-rolling #{i}"
+		puts "RE-rolling #{i}" #THIS IS FOR DEBUGGING ONLY
 			@hand[i] = rand(6) + 1
 		end
 	end
@@ -25,58 +27,87 @@ class Dice_Hand
 		return [3]
 	end
 	
+	#This function determines and returns the strength value of a hand
+	#which is explained in depth in the README
+	def determine_strength
+		strength = 0
+		@hand.sort!
+		@hand.each do |x| # for rank 9
+			strength += x
+		end
+		strength += (@hand[4] * 50) # for rank 8
+		
+	end
+	
+	#This function print the contents of the hand
+	#in an easily readable format
+	def hand_printer
+		puts @name + " hand is:"
+		puts "+---------------------------+"
+		puts "| Die # | 1 | 2 | 3 | 4 | 5 |"
+		puts "|---------------------------|"
+		puts "| Value | #{@hand[0]} | #{@hand[1]} | #{@hand[2]} | #{@hand[3]} | #{@hand[4]} |"
+		puts "+---------------------------+"
+	end
+	
 
 end
 
-#This function takes a dice_hand of five integers representing
-#five dice, and prints them in an easily recognizable format
-def hand_printer(hand, whose)
-	puts whose + " hand is:"
-	puts "+---------------------------+"
-	puts "| Die # | 1 | 2 | 3 | 4 | 5 |"
-	puts "|---------------------------|"
-	puts "| Value | #{hand[0]} | #{hand[1]} | #{hand[2]} | #{hand[3]} | #{hand[4]} |"
-	puts "+---------------------------+"
-end
 
-#This function takes two hand and returns:
-#1 if hand1 wins
-#2 if hand2 wins
-#3 if there is a tie
-def hand_compare(hand1, hand2)
 
+
+#'Main' function
+def play
+	puts "The hands will now be rolled.\n\n"
+
+	your_hand = Dice_Hand.new("Your")
+	your_hand.populate
+	your_hand.hand_printer
+
+	puts "\n"
+
+	opp_hand = Dice_Hand.new("Your opponents")
+	opp_hand.populate
+	opp_hand.hand_printer
+
+	puts "Enter the Die #'s of the dice you wish to re-roll, separated by spaces"
+	select = gets.chomp
+	select = select.split(" ")
+
+	for i in 0..(select.length - 1) do
+		select[i] = select[i].to_i - 1
+	end
+
+	your_hand.reroll(select)
+	opp_hand.reroll(opp_hand.determine_reroll)
+
+	puts "The hands have been rerolled\n\n"
+
+	your_hand.hand_printer
+	opp_hand.hand_printer
+
+	your_strength = your_hand.determine_strength
+	opp_strength = opp_hand.determine_strength
+	
+	if opp_strength > your_strength
+		puts "You lost the round."
+	elsif your_strength > opp_strength
+		puts "You won the round."
+	else
+		puts "The round ends in a tie."
+	end	
+	
+	puts "Would you like to play again (y/n)"
+	again = gets.chomp
+	if again.upcase == "Y"
+		play
+	end
 end
 
 puts "**********Welcome to Dice**********\n\n"
-puts "The hands will now be rolled.\n\n"
+play
 
-your_hand = Dice_Hand.new
-your_hand.populate
-hand_printer(your_hand.instance_variable_get(:@hand), "Your")
 
-puts "\n"
-
-opp_hand = Dice_Hand.new
-opp_hand.populate
-hand_printer(opp_hand.instance_variable_get(:@hand), "Your opponents")
-
-puts "Enter the Die #'s of the dice you wish to re-roll, separated by spaces"
-select = gets.chomp
-select = select.split(" ")
-
-for i in 0..(select.length - 1) do
-	select[i] = select[i].to_i - 1
-end
-
-your_hand.reroll(select)
-opp_hand.reroll(opp_hand.determine_reroll)
-
-puts "The hands have been rerolled\n\n"
-
-hand_printer(your_hand.instance_variable_get(:@hand), "Your")
-hand_printer(opp_hand.instance_variable_get(:@hand), "Your opponents")
-
-result = hand_compare(your_hand.instance_variable_get(:@hand), opp_hand.instance_variable_get(:@hand))
 
 
 
